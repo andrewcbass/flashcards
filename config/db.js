93c228f7d08439e156d6e5fef24b6a4cfdd24472
andrewcbass <andrew.c.bass@gmail.com>
2016-03-16 21:49:38 -0700

    removed search bar from single

--- config/db.js
+++ config/db.js
@@ -2,16 +2,16 @@
 
 var mysql = require('mysql');
 
-//uncomment below for local host
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '1983',
-//   database: 'flashcards'
-// });
+// uncomment below for local host
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1983',
+  database: 'flashcards'
+});
 
-//uncomment below to push to heroku
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// uncomment below to push to heroku
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if(err) {
