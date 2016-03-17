aa98d44318fb384eb0b5d928cc549e951746a0a5
andrewcbass <andrew.c.bass@gmail.com>
2016-03-17 10:40:44 -0700

    change connection to 'or' statement

--- config/db.js
+++ config/db.js
@@ -2,16 +2,13 @@
 
 var mysql = require('mysql');
 
-// uncomment below for local host
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '1983',
-//   database: 'flashcards'
-// });
-
-// uncomment below to push to heroku
- var connection = mysql.createConnection(process.env.JAWSDB_URL);
+//uncomment below for local host
+var connection = mysql.createConnection(process.env.JAWSDB_URL || {
+  host: 'localhost',
+  user: 'root',
+  password: '1983',
+  database: 'flashcards'
+}); //the object portion is just for local host
 
 connection.connect(function(err) {
   if(err) {
