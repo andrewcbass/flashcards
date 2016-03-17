7bb920a75b1fcd129fc2f584a12753d8cf2a2dc9
andrewcbass <andrew.c.bass@gmail.com>
2016-03-16 22:03:04 -0700

    JawsDB MySQL connection

--- config/db.js
+++ config/db.js
@@ -3,15 +3,15 @@
 var mysql = require('mysql');
 
 // uncomment below for local host
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1983',
-  database: 'flashcards'
-});
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '1983',
+//   database: 'flashcards'
+// });
 
 // uncomment below to push to heroku
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if(err) {
