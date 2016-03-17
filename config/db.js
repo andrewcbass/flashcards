cdd9eb4ede6907d3ad2f2b934aa19149034b8267
andrewcbass <andrew.c.bass@gmail.com>
2016-03-16 20:01:44 -0700

    more css and photos added

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@
 
 var mysql = require('mysql');
 
+//uncomment below for local host
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -9,7 +10,8 @@ var connection = mysql.createConnection({
   database: 'flashcards'
 });
 
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+//uncomment below to push to heroku
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if(err) {
